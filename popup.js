17408a80217addbcc72fbb60e5679d8dc2058fa2
Zyad Khan <74572665+ZyadKhan05@users.noreply.github.com>
2024-04-20 13:00:51 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const urlInput = document.getElementById('urlInput');
     const resultDiv = document.getElementById('result');
     const loading = document.getElementById('loader');
+    const badImage = document.getElementById('sad'); 
 
     checkButton.addEventListener('click', function() {
         const url = urlInput.value.trim();
@@ -12,8 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (response) {
                     resultDiv.textContent = `Detected: ${response.positives}, Total: ${response.total}`;
                     loading.classList.add('hide');
+                    if (response.positives > 0) {
+                        badImage.style.display = 'block'; 
+                    } else {
+                        badImage.style.display = 'none'; 
+                    }
                 } else {
                     resultDiv.textContent = 'Failed to get scan results.';
+                    badImage.style.display = 'none'; // Hide the bad image if there's an error
                 }
             });
         }
